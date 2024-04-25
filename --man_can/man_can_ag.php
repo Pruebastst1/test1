6363208a4965c15978796f39af551d28cb832b95
Demoniacchl <62974639+Demoniacchl@users.noreply.github.com>
2024-04-25 15:56:46 -0400

    v2

--- --man_can/man_can_ag.php
+++ --man_can/man_can_ag.php
@@ -1,5 +1,5 @@
-<?php 
-session_start(); 
+<?php
+session_start();
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -7,19 +7,17 @@ session_start();
 <head>
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
-
-  <?php include_once "lib_title.php"; ?>
+  <?php
+  include_once "lib_title.php";
+  ?>
   <meta content="" name="description">
   <meta content="" name="keywords">
-
   <!-- Favicons -->
   <link href="assets/img/favicon.png" rel="icon">
   <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
-
   <!-- Google Fonts -->
   <link href="https://fonts.gstatic.com" rel="preconnect">
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
-
   <!-- Vendor CSS Files -->
   <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
   <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
@@ -41,12 +39,9 @@ session_start();
 </head>
 <body>
   <?php
-
 		   include_once "conex.php"; 
-		 
 		   $link=conectarse();
 		   //mysqli_query("SET NAMES 'utf8'");
-		   
 		   $ses_usu_id     = $_SESSION['ses_id'];
 		   $ses_usu_nombre = $_SESSION['ses_nombre'];
 		   $ses_usu_ape_p  = $_SESSION['ses_ape_p'];
@@ -56,29 +51,22 @@ session_start();
 		   $ses_nivel	   = $_SESSION['ses_nivel'];
 
 		   if ($ses_usu_id==""){
-		         ?> <script>location.href='index.php';</script> <?php
-		   }
-    ?>
-	
-	
-	 
-	
+		         ?> <script>location.href='index.php';</script>
+<?php
+}
+?>
     <script>
         function validarMayusculas(input) {
             input.value = input.value.toUpperCase(); // Convierte el texto a mayúsculas
         }
     </script>
-	
-
   <!-- ======= Header ======= -->
   <?php
   include_once "lib_header.php";
-  include_once "lib_sidebar.php";?>
+  include_once "lib_sidebar.php";
+  ?>
   <!-- End Sidebar-->
-
-
   <main id="main" class="main">
-
     <div class="pagetitle">
       <h1>Mantención de Materias</h1>
       <nav>
@@ -90,27 +78,20 @@ session_start();
         </ol>
       </nav>
     </div><!-- End Page Title -->
-
     <section class="section">
       <div class="row">
         <div class="col-lg-12">
-
           <div class="card">
             <div class="card-body">
               <h5 class="card-title">Canales</h5>
-
               <!-- General Form Elements -->
-              <form name="FormCan" id="FormCan2" enctype="multipart/form-data">
-			  
-			     
-                
+              <form name="FormCan" id="FormCan2" enctype="multipart/form-data">         
 				<div class="row mb-3">
                   <label for="inputText" class="col-sm-2 col-form-label">Nombre</label>
                   <div class="col-sm-10">
                     <input type="text" class="form-control" name="f_nombre"   oninput="validarMayusculas(this)" required>
                   </div>
                 </div>
-
                 <div class="row mb-3">
                   <label class="col-sm-2 col-form-label"> </label>
                   <div class="col-sm-10">
@@ -119,25 +100,19 @@ session_start();
 					<a href="man_can.php" class="btn btn-danger" >Volver</a>
                   </div>
                 </div>
-
               </form><!-- End General Form Elements -->
-
             </div>
           </div>
-
         </div>
-
       </div>
     </section>
-
   </main><!-- End #main -->
-
   <!-- ======= Footer ======= -->
-  <?php include_once "lib_footer.php"; ?>
+  <?php
+  include_once "lib_footer.php";
+  ?>
   <!-- End Footer -->
-
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
-
   <!-- Vendor JS Files -->
   <script src="assets/vendor/bootstrap/js/bootstrap.bundle.js"></script>
   <script src="assets/vendor/php-email-form/validate.js"></script>
@@ -155,10 +130,8 @@ session_start();
         // Handler for form submission
         $('#FormCan2').submit(function(e) {
             e.preventDefault(); // Evita la presentación del formulario
-
             // Guarda una referencia al formulario
             var formData = new FormData(this);
-
             // Tu llamada AJAX aquí
             $.ajax({
                 url: 'man_can_p.php', // URL a tu script PHP de manejo
@@ -178,7 +151,6 @@ session_start();
                             backround: "#388E3C",
                             className: "info",
                         }).showToast();
-
                         $('#FormCan2')[0].reset(); // Vaciar el formulario
                     } else {
                         alert("Error: " + response.message); // Mostrar mensaje de error
@@ -193,7 +165,5 @@ session_start();
         });
     });
 </script>
-
 </body>
-
 </html>
